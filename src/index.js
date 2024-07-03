03227a691fc0166d24c49c8ee1bc6ed0f77199e3
Taejin Kim <rlaxowls1316@naver.com>
2024-07-03 15:04:48 +0900

    cors, helmet, nodemon 적용

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import dayjs from 'dayjs';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
 
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("Express");
+//미들웨어
+app.use(cors({
+    origin: '*',
+}))
+
+app.use(helmet());
+
+const today = new Date();
+
+const todayToDayjs = dayjs(today).format('YYYY-MM-DD');
+console.log({todayToDayjs})
+
+const password = '1234';
+const hashedPassword = bcrypt.hashSync(password,10);
+console.log(hashedPassword);
+
+const token = jwt.sign('1234',"iwoakvneowh42")
+
+console.log(token)
+
+app.get("/",(req, res)=>{
+    res.send("Node.js 강의 재밌어요!");
 })
 
-app.listen(8000, ()=> {
+app.listen(8000, ()=>{
     console.log('서버가 시작되었습니다.');
 })
